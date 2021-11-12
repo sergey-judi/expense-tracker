82ff3511b9a8e268b0aaf8d343e68aff525b7d4c
“sergey-judi” <serega7qwerty@gmail.com>
2021-11-12 10:46:19 +0200

    ET-12: bootstrapped exceptions handling

---
+++ src/main/java/com/expensetracker/exception/model/ErrorResponse.java
@@ -0,0 +1,4 @@
+package com.expensetracker.exception.model;
+
+public class ErrorResponse {
+}
---
+++ src/main/java/com/expensetracker/web/ErrorHandler.java
@@ -0,0 +1,7 @@
+package com.expensetracker.web;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+}
