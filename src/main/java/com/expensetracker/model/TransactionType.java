fe282448a7198f793aef340b2b05466c2de6e1f9
“sergey-judi” <serega7qwerty@gmail.com>
2021-11-14 14:55:16 +0200

    ET-3: added transaction-category chain & custom annotation to check transaction type correctness

--- src/main/java/com/expensetracker/model/TransactionType.java
+++ src/main/java/com/expensetracker/model/TransactionType.java
@@ -1,6 +1,7 @@
 package com.expensetracker.model;
 
 import com.expensetracker.exception.UnsupportedTransactionTypeException;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 import static java.util.Objects.isNull;
 
@@ -8,12 +9,13 @@ public enum TransactionType {
   DEBIT("debit"),
   CREDIT("credit");
 
-  private String type;
+  private final String type;
 
   TransactionType(String type) {
     this.type = type;
   }
 
+  @JsonValue
   public String getCanonicalType() {
     return this.type;
   }
@@ -24,7 +26,7 @@ public enum TransactionType {
     }
 
     for (TransactionType transactionType : values()) {
-      if (transactionType.getCanonicalType().equals(type)) {
+      if (transactionType.getCanonicalType().equals(type.toLowerCase())) {
         return transactionType;
       }
     }
